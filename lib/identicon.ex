2cf3af36f765b36301075a31ee298d22236d9dc7
dave doolin <david.doolin@gmail.com>
2018-12-09 06:09:17 -0800

    video 39 struct for image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -18,7 +18,9 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list(hash)
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
