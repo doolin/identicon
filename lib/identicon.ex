c9298c3e65213fc6bf6b6bbd331588853e3f047e
dave doolin <david.doolin@gmail.com>
2018-12-13 04:25:18 -0800

    video 44, Enum.map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -13,7 +13,7 @@ defmodule Identicon do
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
     |> Enum.chunk(3)
-    |> mirror_row
+    |> Enum.map(&mirror_row/1)
   end
 
   @doc """
